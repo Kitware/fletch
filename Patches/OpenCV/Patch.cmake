846d8431defc16557d4bea3037e957413b08c131
Matt Leotta <matt.leotta@kitware.com>
2016-01-21 15:39:39 -0500

    Patch OpenCV to remove INSTALL_NAME_DIR line for modules.
    On OS X, the OpenCV libraries have install names prefixed with "lib"
    and none of them can be found when using fixup_bundle to produce a
    package.  Removing the line that sets INSTALL_NAME_DIR fixes the
    problem.

--- Patches/OpenCV/Patch.cmake
+++ Patches/OpenCV/Patch.cmake
@@ -8,3 +8,7 @@ message("Patching OpenCV in ${OpenVC_source}")
 file(COPY ${OpenCV_patch}/CMakeLists.txt
   DESTINATION ${OpenCV_source}/modules/features2d/
 )
+
+file(COPY ${OpenCV_patch}/OpenCVModule.cmake
+  DESTINATION ${OpenCV_source}/cmake/
+)
