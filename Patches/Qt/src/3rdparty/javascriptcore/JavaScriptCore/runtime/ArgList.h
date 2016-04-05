f5a184d9522e6237c555a50a50c2f3b9fea2e2ac
David Stoup <david.stoup@kitware.com>
2016-04-05 16:12:07 -0400

    Define operator new and delete to fix compiler errors in Debug mode.

--- Patches/Qt/src/3rdparty/javascriptcore/JavaScriptCore/runtime/ArgList.h
+++ Patches/Qt/src/3rdparty/javascriptcore/JavaScriptCore/runtime/ArgList.h
@@ -171,8 +171,8 @@ namespace JSC {
         void* operator new[](size_t);
         void operator delete[](void*);
 
-		void* operator new(size_t s, void*v);
-        void operator delete(void* v, size_t);
+		void* operator new(size_t s, void*v){ ::operator new(s,v); }
+        void operator delete(void* v, size_t){ if (v != 0 ) ::operator delete(v); }
     };
 
     class ArgList {
