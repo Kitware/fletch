63ac7d914f449c844f1d841686bdfd6129004b50
Matt Leotta <matt.leotta@kitware.com>
2017-02-02 14:43:41 -0500

    added patching for OpenCV contrib

--- CMake/External_OpenCV_contrib.cmake
+++ CMake/External_OpenCV_contrib.cmake
@@ -1,3 +1,8 @@
+set(OPENCV_CONTRIB_PATCH_COMMAND ${CMAKE_COMMAND}
+  -DOpenCV_contrib_patch:PATH=${fletch_SOURCE_DIR}/Patches/OpenCV_contrib
+  -DOpenCV_contrib_source:PATH=${fletch_BUILD_PREFIX}/src/OpenCV_contrib
+  -P ${fletch_SOURCE_DIR}/Patches/OpenCV_contrib/Patch.cmake)
+
 # The OpenCV contrib repo external project
 ExternalProject_Add(OpenCV_contrib
   URL ${OpenCV_contrib_url}
@@ -12,6 +17,7 @@ ExternalProject_Add(OpenCV_contrib
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
+  PATCH_COMMAND ${OPENCV_CONTRIB_PATCH_COMMAND}
   )
 
 set(OpenCV_contrib_MODULE_PATH "${fletch_BUILD_PREFIX}/src/OpenCV_contrib/modules")
---
+++ Patches/OpenCV_contrib/Patch.cmake
@@ -0,0 +1,6 @@
+#+
+# This file is called as CMake -P script for the patch step of
+# External_OpenCV_contrib.cmake when building with OpenCV contrib
+#-
+
+message("Patching OpenCV contrib in ${OpenCV_contrib_source}")
