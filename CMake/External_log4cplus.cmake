66ec35ea653eb25c0a892f34298557d35a468968
Matt Leotta <matt.leotta@kitware.com>
2017-06-27 10:35:26 -0400

    added missing LOG4CPLUS_DIR to FletchConfig.cmake

--- CMake/External_log4cplus.cmake
+++ CMake/External_log4cplus.cmake
@@ -16,11 +16,13 @@ ExternalProject_Add(log4cplus
 fletch_external_project_force_install(PACKAGE log4cplus)
 
 set(LOG4CPLUS_ROOT ${fletch_BUILD_INSTALL_PREFIX} CACHE STRING "")
+set(LOG4CPLUS_DIR "${LOG4CPLUS_ROOT}/lib/cmake/log4cplus" CACHE PATH "" FORCE)
 
 file(APPEND ${fletch_CONFIG_INPUT} "
 ########################################
 # log4cplus
 ########################################
 set(LOG4CPLUS_ROOT    @LOG4CPLUS_ROOT@)
+set(LOG4CPLUS_DIR    @LOG4CPLUS_DIR@)
 set(fletch_ENABLED_log4cplus TRUE)
 ")
