9c09c09f07b88fae48884653870de68276eb8c2f
David Stoup <david.stoup@kitware.com>
2017-07-17 09:33:03 -0400

    Configure fletch_ROOT based on whether this is a build or an install.

--- CMake/fletch-install.cmake
+++ CMake/fletch-install.cmake
@@ -4,5 +4,25 @@
 
 install(DIRECTORY ${fletch_BUILD_INSTALL_PREFIX}/ DESTINATION ${CMAKE_INSTALL_PREFIX})
 
+
+set(fletch_INSTALL_BUILD_DIR ${CMAKE_INSTALL_PREFIX})
+set(fletch_CONFIG_OUTPUT ${CMAKE_INSTALL_PREFIX}/lib/cmake/fletch/fletchConfig.cmake )
 # Also install the Config file so it can be found.
-install(FILES ${fletch_CONFIG_OUTPUT} DESTINATION ${CMAKE_INSTALL_PREFIX})
+
+
+# We need to configure the 'install' version of the config into an 'export' directory, then install it.
+# The export directory was chosen because it make sense semantically as is not likely to confuse users.
+#
+configure_file(
+  ${fletch_CONFIG_INPUT} ${fletch_BUILD_PREFIX}/config/export/fletchConfig_install.cmake
+  @ONLY
+  )
+
+install(
+  FILES
+  ${fletch_BUILD_PREFIX}/config/export/fletchConfig_install.cmake
+  DESTINATION
+  ${CMAKE_INSTALL_PREFIX}/lib/cmake/fletch/
+  RENAME
+  fletchConfig.cmake
+  )
