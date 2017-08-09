5a9a950b3fd4f6dc763fc2d18859761216a746a7
joncrall <jon.crall@kitware.com>
2017-08-09 14:26:47 -0400

    Versioned ffmpeg.
    Default option is the stable 2.6.2, but 3.3.3 is now exposed.
    Windows default is unchanged.

--- Patches/FFmpeg/2.6.2/Patch.cmake
+++ Patches/FFmpeg/2.6.2/Patch.cmake
@@ -4,6 +4,8 @@
 #-
 
 
+# Adds patch to define __STDC_CONSTANT_MACROS and include <stdint.h>
+# This patch is for non-windows systems
 configure_file(
   ${FFmpeg_patch}/common.h
   ${FFmpeg_source}/libavutil/common.h
