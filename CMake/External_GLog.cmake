f58d82555a0c756dc5fcab09411b1649e4ea2289
David Stoup <david.stoup@kitware.com>
2017-09-22 12:10:01 -0400

    Convert the External_GLog file to use the CMake generator.

--- CMake/External_GLog.cmake
+++ CMake/External_GLog.cmake
@@ -1,33 +1,19 @@
 
-if (WIN32)
-
-  # Build option for windows not yet generated
-  message( FATAL_ERROR "GLog on windows not yet supported" )
-
-else ()
-
-  Fletch_Require_Make()
-
-  ExternalProject_Add(GLog
-    URL ${GLog_file}
-    URL_MD5 ${GLog_md5}
-    DOWNLOAD_NAME ${GLog_dlname}
-    PREFIX ${fletch_BUILD_PREFIX}
-    DOWNLOAD_DIR ${fletch_DOWNLOAD_DIR}
-    INSTALL_DIR ${fletch_BUILD_INSTALL_PREFIX}
-    PATCH_COMMAND ${CMAKE_COMMAND}
-      -DGLog_patch:PATH=${fletch_SOURCE_DIR}/Patches/GLog
-      -DGLog_source:PATH=${fletch_BUILD_PREFIX}/src/GLog
-      -P ${fletch_SOURCE_DIR}/Patches/GLog/Patch.cmake
-    BUILD_IN_SOURCE 1
-    CONFIGURE_COMMAND
-      ${fletch_BUILD_PREFIX}/src/GLog/configure
-      --prefix=${fletch_BUILD_INSTALL_PREFIX}
-      --enable-shared
-    BUILD_COMMAND ${MAKE_EXECUTABLE}
-    INSTALL_COMMAND ${MAKE_EXECUTABLE} install
-    )
-endif ()
+ExternalProject_Add(GLog
+  URL ${GLog_file}
+  URL_MD5 ${GLog_md5}
+  DOWNLOAD_NAME ${GLog_dlname}
+  PREFIX ${fletch_BUILD_PREFIX}
+  DOWNLOAD_DIR ${fletch_DOWNLOAD_DIR}
+  INSTALL_DIR ${fletch_BUILD_INSTALL_PREFIX}
+  PATCH_COMMAND ${CMAKE_COMMAND}
+    -DGLog_patch:PATH=${fletch_SOURCE_DIR}/Patches/GLog
+    -DGLog_source:PATH=${fletch_BUILD_PREFIX}/src/GLog
+    -P ${fletch_SOURCE_DIR}/Patches/GLog/Patch.cmake
+  CMAKE_GENERATOR ${gen}
+  CMAKE_ARGS
+    ${COMMON_CMAKE_ARGS}
+  )
 
 fletch_external_project_force_install(PACKAGE GLog)
 
