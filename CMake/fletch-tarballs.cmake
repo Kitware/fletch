41d06f6173c614e82e88ba8fea5c70db4cfc7648
David Stoup <david.stoup@kitware.com>
2017-09-25 07:14:48 -0400

    Fix tarballs, bad merge.

--- CMake/fletch-tarballs.cmake
+++ CMake/fletch-tarballs.cmake
@@ -181,14 +181,15 @@ list(APPEND fletch_external_sources log4cplus)
 # GFlags
 set(GFlags_version "2.2.1")
 set(GFlags_url "https://github.com/gflags/gflags/archive/v${GFlags_version}.tar.gz")
-set(GFlags_md5 "ac432de923f9de1e9780b5254884599f")
-set(GFlags_dlname "gflags-${GGFlags_version}.tar.gz")
+set(GFlags_md5 "b98e772b4490c84fc5a87681973f75d1")
+set(GFlags_dlname "gflags-${GFlags_version}.tar.gz")
 list(APPEND fletch_external_sources GFlags)
 
 # GLog
 set(GLog_version "0.3.5")
 set(GLog_url "https://github.com/google/glog/archive/v${GLog_version}.tar.gz")
 set(GLog_md5 "5df6d78b81e51b90ac0ecd7ed932b0d4")
+set(GLog_dlname "glog-${GLog_version}.tar.gz")
 list(APPEND fletch_external_sources GLog)
 
 set(GTest_version "1.8.0")
