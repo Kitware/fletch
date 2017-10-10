b2298b5d025442bed6733dfa317da0344da242dd
David Stoup <david.stoup@kitware.com>
2017-10-10 08:50:19 -0400

    Patch build.bat. Set env to accomodate vcvarsall.bat changing directory

--- Patches/Boost/build.bat
+++ Patches/Boost/build.bat
@@ -5,6 +5,9 @@ REM ~ Distributed under the Boost Software License, Version 1.0.
 REM ~ (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 
 setlocal
+
+set VSCMD_START_DIR=%CD%
+
 goto Start
 
 
