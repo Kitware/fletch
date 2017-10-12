80dcad23f4c17176ac5fca3eeec480ae61130978
David Stoup <david.stoup@kitware.com>
2017-10-12 10:45:12 -0400

    Patch atomicops.h

--- Patches/Protobuf/2.5.0/Patch.cmake
+++ Patches/Protobuf/2.5.0/Patch.cmake
@@ -6,3 +6,16 @@
 file(COPY ${Protobuf_PATCH_DIR}/platform_macros.h
   DESTINATION ${Protobuf_SOURCE_DIR}/src/google/protobuf/stubs/
   )
+
+file(COPY ${Protobuf_PATCH_DIR}/atomicops.h
+  DESTINATION ${Protobuf_SOURCE_DIR}/src/google/protobuf/stubs/
+  )
+
+
+file(COPY ${Protobuf_PATCH_DIR}/atomicops_internals_arm_gcc.h
+  DESTINATION ${Protobuf_SOURCE_DIR}/src/google/protobuf/stubs/
+  )
+
+file(COPY ${Protobuf_PATCH_DIR}/atomicops_internals_arm64_gcc.h
+  DESTINATION ${Protobuf_SOURCE_DIR}/src/google/protobuf/stubs/
+  )
