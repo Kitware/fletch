4ca947077825287b0d41aeb08f87e43d93c306bf
David Stoup <david.stoup@kitware.com>
2017-11-03 16:18:06 -0400

    Apply VTK patch to expand gcc version support

--- Patches/VTK/6.2/CMake/GenerateExportHeader.cmake
+++ Patches/VTK/6.2/CMake/GenerateExportHeader.cmake
@@ -166,12 +166,13 @@ macro(_test_compiler_hidden_visibility)
     execute_process(COMMAND ${CMAKE_C_COMPILER} ARGS --version
       OUTPUT_VARIABLE _gcc_version_info
       ERROR_VARIABLE _gcc_version_info)
-    string(REGEX MATCH "[345]\\.[0-9]\\.[0-9]*"
+
+    string(REGEX MATCH "[34567]\\.[0-9]\\.[0-9]*"
       _gcc_version "${_gcc_version_info}")
     # gcc on mac just reports: "gcc (GCC) 3.3 20030304 ..." without the
     # patch level, handle this here:
     if(NOT _gcc_version)
-      string(REGEX REPLACE ".*\\(GCC\\).*([34]\\.[0-9]).*" "\\1.0"
+      string(REGEX REPLACE ".*\\(GCC\\).*([34567]\\.[0-9]).*" "\\1.0"
         _gcc_version "${_gcc_version_info}")
     endif()
 
