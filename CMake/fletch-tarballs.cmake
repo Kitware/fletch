d96bb039deecd68dc4e223f73c000886c7d66dfe
joncrall <jon.crall@kitware.com>
2018-02-28 19:47:08 -0500

    update eigen version

--- CMake/fletch-tarballs.cmake
+++ CMake/fletch-tarballs.cmake
@@ -148,9 +148,9 @@ if(_FFmpeg_supported)
 endif()
 
 # EIGEN
-set(Eigen_version 3.2.9)
+set(Eigen_version 3.3.4)
 set(Eigen_url "http://bitbucket.org/eigen/eigen/get/${Eigen_version}.tar.gz")
-set(Eigen_md5 "6a578dba42d1c578d531ab5b6fa3f741")
+set(Eigen_md5 "1a47e78efe365a97de0c022d127607c3")
 set(Eigen_dlname "eigen-${Eigen_version}.tar.gz")
 list(APPEND fletch_external_sources Eigen)
 
