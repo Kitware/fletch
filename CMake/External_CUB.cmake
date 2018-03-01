471af21ddf66914f0e2e31c9d530fc2094302e71
joncrall <jon.crall@kitware.com>
2018-03-01 12:53:57 -0500

    Fixed CUB name

--- CMake/External_CUB.cmake
+++ CMake/External_CUB.cmake
@@ -1,8 +1,8 @@
 ExternalProject_Add(CUB
 
-  DEPENDS ${CUB2_DEPENDS}
-  URL ${CUB2_url}
-  URL_MD5 ${CUB2_md5}
+  DEPENDS ${CUB_DEPENDS}
+  URL ${CUB_url}
+  URL_MD5 ${CUB_md5}
   PREFIX ${fletch_BUILD_PREFIX}
   DOWNLOAD_DIR ${fletch_DOWNLOAD_DIR}
   INSTALL_DIR ${fletch_BUILD_INSTALL_PREFIX}
@@ -13,11 +13,11 @@ ExternalProject_Add(CUB
 
 fletch_external_project_force_install(PACKAGE CUB)
 
-set(CUB2_ROOT ${fletch_BUILD_INSTALL_PREFIX} CACHE STRING "")
+set(CUB_ROOT ${fletch_BUILD_INSTALL_PREFIX} CACHE STRING "")
 
 file(APPEND ${fletch_CONFIG_INPUT} "
 ########################################
 # CUB
 ########################################
-set(CUB2_ROOT    \${fletch_ROOT})
+set(CUB_ROOT    \${fletch_ROOT})
 ")
