ae94a06531f04ce18664fb91ee028ad598b01a1e
Matthew Woehlke <matthew.woehlke@kitware.com>
2018-05-22 13:10:44 -0400

    Import files for patching GeographicLib
    Create "patches" for GeographicLib. These are currently the original
    files from upstream, so that the next commit will show what changes we
    are actually making.

--- Patches/GeographicLib/Patch.cmake
+++ Patches/GeographicLib/Patch.cmake
@@ -5,3 +5,12 @@
 file(COPY ${GeographicLib_patch}/MagneticField.pod
   DESTINATION ${GeographicLib_source}/man
 )
+file(COPY ${GeographicLib_patch}/CMakeLists.txt
+  DESTINATION ${GeographicLib_source}
+)
+file(COPY ${GeographicLib_patch}/cmake/CMakeLists.txt
+  DESTINATION ${GeographicLib_source}/cmake
+)
+file(COPY ${GeographicLib_patch}/src/CMakeLists.txt
+  DESTINATION ${GeographicLib_source}/src
+)
