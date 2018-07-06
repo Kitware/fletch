3454f57e7dfc557167f5e3db1cf4aa559fa532e7
David Stoup <david.stoup@kitware.com>
2018-07-06 17:08:29 -0400

    Patch moc to fix problem in QT_VERSION_CHECK macro

--- Patches/Qt/4.8.6/src/tools/moc/main.cpp
+++ Patches/Qt/4.8.6/src/tools/moc/main.cpp
@@ -190,6 +190,10 @@ int runMoc(int _argc, char **_argv)
 
     // Workaround a bug while parsing the boost/type_traits/has_operator.hpp header. See QTBUG-22829
     pp.macros["BOOST_TT_HAS_OPERATOR_HPP_INCLUDED"];
+    pp.macros["BOOST_LEXICAL_CAST_INCLUDED"];
+    pp.macros["BOOST_NEXT_PRIOR_HPP_INCLUDED"];
+    pp.macros["BOOST_TYPE_TRAITS_HPP"];
+    pp.macros["_SYS_SYSMACROS_H_OUTER"];
 
     QByteArray filename;
     QByteArray output;
