ea9c725283263175b53dcc3be35e3525db6c164f
Matt Dawkins <matt.dawkins@kitware.com>
2018-12-28 14:13:59 -0500

    Update vxl version

--- CMake/fletch-tarballs.cmake
+++ CMake/fletch-tarballs.cmake
@@ -421,9 +421,9 @@ endif()
 list(APPEND fletch_external_sources VTK)
 
 # VXL
-set(VXL_version "1613dd9f8f06dae759d597c7e86f552a1d539754")
-set(VXL_url "https://github.com/vxl/vxl/archive/${VXL_version}.zip")
-set(VXL_md5 "f49b704ffc2f5146d303f5b40e977e79")
+set(VXL_version "2.0.2")
+set(VXL_url "https://github.com/vxl/vxl/archive/v${VXL_version}.zip")
+set(VXL_md5 "a32c1d69db6b1de605e010bd14cb6524")
 set(VXL_dlname "vxl-${VXL_version}.zip")
 list(APPEND fletch_external_sources VXL)
 
