aa967b0fa088e9605cf2c4a5b4b83a5ad29daff3
David Stoup <david.stoup@kitware.com>
2019-03-18 10:29:24 -0400

    Force gtest to build c++ 98 for gcc less than 4.9

--- CMake/External_GTest.cmake
+++ CMake/External_GTest.cmake
@@ -1,3 +1,12 @@
+
+if(CMAKE_CXX_COMPILER_ID MATCHES GNU)
+  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.9)
+    set(GTest_CXX_STANDARD "98")
+  else()
+    set(GTest_CXX_STANDARD "11")
+  endif()
+endif()
+
 ExternalProject_Add(GTest
   URL ${GTest_url}
   URL_MD5 ${GTest_md5}
@@ -10,6 +19,7 @@ ExternalProject_Add(GTest
     -P ${fletch_SOURCE_DIR}/Patches/GTest/Patch.cmake
   CMAKE_ARGS
     ${COMMON_CMAKE_ARGS}
+    -DCMAKE_CXX_STANDARD:STRING=${GTest_CXX_STANDARD}
   )
 
 set(GTEST_ROOT ${fletch_BUILD_INSTALL_PREFIX} CACHE STRING "")
