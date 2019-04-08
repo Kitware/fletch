d4e34546b67459a1d7329dde54b2c016e44321bf
David Stoup <david.stoup@kitware.com>
2019-04-08 14:11:11 -0400

    Set up patch to fix Qt5 build error in gcc 8.3

--- Patches/Qt/5.11.2/Patch.cmake
+++ Patches/Qt/5.11.2/Patch.cmake
@@ -9,3 +9,8 @@ message("Patching Qt in ${Qt_source}")
 file(COPY ${Qt_patch}/qtconnectivity/src/bluetooth/qbluetoothservicediscoveryagent_winrt.cpp
   DESTINATION ${Qt_source}/qtconnectivity/src/bluetooth/
   )
+
+# Qt asm declaration broken on gcc gcc 8.3
+file(COPY ${Qt_patch}/qtscript/src/3rdparty/javascriptcore/JavaScriptCore/jit/JITStubs.cpp
+  DESTINATION ${Qt_source}/qtscript/src/3rdparty/javascriptcore/JavaScriptCore/jit/
+  )
