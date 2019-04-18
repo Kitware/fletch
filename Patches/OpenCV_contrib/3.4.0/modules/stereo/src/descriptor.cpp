cc4f2c432b05b676e00d47ee3f7eccaabe58378d
David Stoup <david.stoup@kitware.com>
2019-04-18 14:36:47 -0400

    Patch opencv_contrib. Don't compare pointer to int

--- Patches/OpenCV_contrib/3.4.0/modules/stereo/src/descriptor.cpp
+++ Patches/OpenCV_contrib/3.4.0/modules/stereo/src/descriptor.cpp
@@ -226,8 +226,8 @@ namespace cv
         //integral image computation used in the Mean Variation Census Transform
         void imageMeanKernelSize(const Mat &image, int windowSize, Mat &cost)
         {
-            CV_Assert(image.size > 0);
-            CV_Assert(cost.size > 0);
+            CV_Assert(image.size != NULL);
+            CV_Assert(cost.size != NULL);
             CV_Assert(windowSize % 2 != 0);
             int win = windowSize / 2;
             float scalling = ((float) 1) / (windowSize * windowSize);
