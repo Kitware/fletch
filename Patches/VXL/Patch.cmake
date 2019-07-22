ed3ef1a00bc7ebea6b826101c71358cc22566f8c
Matt Dawkins <matt.dawkins@kitware.com>
2019-07-22 16:02:12 -0400

    Revert "Add patch for windows linking issues"
    This reverts commit d5bbf06fcd8d9d34057f0623ae2f3162f1371049.

--- Patches/VXL/Patch.cmake
+++ Patches/VXL/Patch.cmake
@@ -8,15 +8,3 @@ message("Patching VXL in ${VXL_source}")
 file(COPY ${VXL_patch}//vidl_ffmpeg_ostream_v56.hxx
   DESTINATION ${VXL_source}/core/vidl/
 )
-
-file(COPY ${VXL_patch}//vnl_matrix_fixed.h
-  DESTINATION ${VXL_source}/core/vnl/
-)
-
-file(COPY ${VXL_patch}//vnl_io_matrix.h
-  DESTINATION ${VXL_source}/core/vnl/
-)
-
-file(COPY ${VXL_patch}//vnl_io_matrix.hxx
-  DESTINATION ${VXL_source}/core/vnl/io/
-)
