4d65be422ae7d88be347623b39b826b78522048c
Neal Siekierski <neal.siekierski@kitware.com>
2019-08-09 12:39:52 -0400

    Update qtExtensions to include a Windows compilation fix
    This adds commit 788d8998, "Fix propagation of QTE_REQUIRED_CXX_FLAGS".

--- CMake/fletch-tarballs.cmake
+++ CMake/fletch-tarballs.cmake
@@ -566,10 +566,10 @@ set(YAMLcpp_dlname "yaml-cpp-release-${YAMLcpp_version}.tar.gz")
 list(APPEND fletch_external_sources YAMLcpp)
 
 # qtExtensions
-set(qtExtensions_version "20190517gita911d919")
-set(qtExtensions_tag "a911d919bc8f01c6f3af067dc95a654b2f27cf16")
+set(qtExtensions_version "20190808git788d8998")
+set(qtExtensions_tag "788d8998e6e43b394898bbae4f502cb2cd5bfde9")
 set(qtExtensions_url "https://github.com/Kitware/qtextensions/archive/${qtExtensions_tag}.tar.gz")
-set(qtExtensions_md5 "b4be0f7cf4b3bde943fc1d47061fa82a")
+set(qtExtensions_md5 "5dfd7fe3424c8dcdace9c69f4672441c")
 set(qtExtensions_dlname "qtExtensions-${qtExtensions_version}.tar.gz")
 list(APPEND fletch_external_sources qtExtensions)
 
