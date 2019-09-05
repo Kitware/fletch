4f1977bfa4330a94974ec26530af43f6d48e1db9
Matthew Woehlke <matthew.woehlke@kitware.com>
2019-09-05 13:44:38 -0400

    Update qtExtensions
    Update to latest qtExtensions, which adds some qtUiState enhancements
    and fixes for using Q_ENUM_NS.

--- CMake/fletch-tarballs.cmake
+++ CMake/fletch-tarballs.cmake
@@ -567,10 +567,10 @@ set(YAMLcpp_dlname "yaml-cpp-release-${YAMLcpp_version}.tar.gz")
 list(APPEND fletch_external_sources YAMLcpp)
 
 # qtExtensions
-set(qtExtensions_version "20190517gita911d919")
-set(qtExtensions_tag "a911d919bc8f01c6f3af067dc95a654b2f27cf16")
+set(qtExtensions_version "20190905git873c0676")
+set(qtExtensions_tag "873c06769a2d76e2323152efc40ad910717ce648")
 set(qtExtensions_url "https://github.com/Kitware/qtextensions/archive/${qtExtensions_tag}.tar.gz")
-set(qtExtensions_md5 "b4be0f7cf4b3bde943fc1d47061fa82a")
+set(qtExtensions_md5 "f7b617250040e2e4bffa0e2a0bd93c89")
 set(qtExtensions_dlname "qtExtensions-${qtExtensions_version}.tar.gz")
 list(APPEND fletch_external_sources qtExtensions)
 
