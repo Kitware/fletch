e3f543b1fa7f7c5b5c294e63b72f5e02a7d571be
Matt Dawkins <matt.dawkins@kitware.com>
2019-11-12 18:57:13 -0500

    Rename files

---
+++ Patches/CPython/install_pip_windows.cmake
@@ -0,0 +1,16 @@
+message("Running CPython install")
+
+function( CopyFiles _inRegex _outDir )
+  file( GLOB FILES_TO_COPY ${_inRegex} )
+  if( FILES_TO_COPY )
+    file( COPY ${FILES_TO_COPY} DESTINATION ${_outDir} )
+  endif()
+endfunction()
+
+CopyFiles( ${CPYTHON_BUILD_LOC}/Include/*.h ${fletch_BUILD_INSTALL_PREFIX}/include )
+CopyFiles( ${CPYTHON_BUILD_LOC}/PC/*.h ${fletch_BUILD_INSTALL_PREFIX}/include )
+CopyFiles( ${CPYTHON_BUILD_LOC}/PCbuild/*/*.exe ${fletch_BUILD_INSTALL_PREFIX}/bin )
+CopyFiles( ${CPYTHON_BUILD_LOC}/PCbuild/*/*.dll ${fletch_BUILD_INSTALL_PREFIX}/bin )
+CopyFiles( ${CPYTHON_BUILD_LOC}/PCbuild/*/*.lib ${fletch_BUILD_INSTALL_PREFIX}/lib )
+CopyFiles( ${CPYTHON_BUILD_LOC}/PCbuild/*/*.pyd ${PYTHON_BASEPATH}/site-packages )
+CopyFiles( ${CPYTHON_BUILD_LOC}/Lib/* ${PYTHON_BASEPATH}/site-packages )
