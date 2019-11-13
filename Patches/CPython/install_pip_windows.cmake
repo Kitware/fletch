56cbfdc8f2d5404728e3322045fb97c8913e0959
Matt Dawkins <matt.dawkins@kitware.com>
2019-11-13 00:56:06 -0500

    Missing bracket

--- Patches/CPython/install_pip_windows.cmake
+++ Patches/CPython/install_pip_windows.cmake
@@ -7,7 +7,7 @@ function( CopyFiles _inRegex _outDir )
   endif()
 endfunction()
 
-set( OUTPUT_LIB_DIR ${INSTALL_DIRECTORY}/lib/python${PYTHON_MAJOR}.${PYTHON_MINOR}/site-packages
+set( OUTPUT_LIB_DIR ${INSTALL_DIRECTORY}/lib/python${PYTHON_MAJOR}.${PYTHON_MINOR}/site-packages )
 
 CopyFiles( ${SOURCE_DIRECTORY}/Scripts/*.exe ${INSTALL_DIRECTORY}/bin )
 CopyFiles( ${SOURCE_DIRECTORY}/Lib/site-packages/* ${OUTPUT_LIB_DIR} )
