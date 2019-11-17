884cbdf4e8e27c43ab607fdd6c0c98724fe70e35
Matt Dawkins <matt.dawkins@kitware.com>
2019-11-17 01:30:32 -0500

    Add cython to base install

--- CMake/External_CPython.cmake
+++ CMake/External_CPython.cmake
@@ -127,8 +127,8 @@ set( PYTHON_LIBRARY_DEBUG ${PYTHON_LIBRARY_DEBUG} )
 
 # --------------------- ADD ANY EXTRA PYTHON LIBS HERE -------------------------
 
-set( fletch_PYTHON_LIBS numpy matplotlib )
-set( fletch_PYTHON_LIB_CMDS "numpy" "matplotlib" )
+set( fletch_PYTHON_LIBS numpy matplotlib cython )
+set( fletch_PYTHON_LIB_CMDS "numpy" "matplotlib" "Cython" )
 
 if( NOT WIN32 )
   set( fletch_PYTHON_LIBS wheel )
