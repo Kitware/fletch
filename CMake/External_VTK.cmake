93b447b72bb4a460aaf1f478cc08304840f3f36f
joncrall <erotemic@gmail.com>
2019-11-18 14:44:42 -0500

    Add option to enable/disable VTK Python wrappings

--- CMake/External_VTK.cmake
+++ CMake/External_VTK.cmake
@@ -187,24 +187,27 @@ set(vtk_cmake_args ${vtk_cmake_args}
   )
 
 # PYTHON
-if (fletch_BUILD_WITH_PYTHON AND NOT MSVC14 )
+if(fletch_BUILD_WITH_PYTHON AND NOT MSVC14 )
+  option(fletch_ENABLE_VTK_PYTHON "Enable Python wrappings for VTK" ON)
   find_package(PythonInterp)
   find_package(PythonLibs)
 
-  if(PythonInterp_FOUND AND PythonLibs_FOUND)
+  if(PythonInterp_FOUND AND PythonLibs_FOUND AND fletch_ENABLE_VTK_PYTHON)
     set(VTK_WRAP_PYTHON ON)
     message(STATUS "VTK building with python support")
   else()
     set(VTK_WRAP_PYTHON OFF)
-    message(WARNING "VTK building without python support. Python NOT found.")
+    if(fletch_ENABLE_VTK_PYTHON)
+      message(WARNING "VTK building without python support. Python NOT found.")
+    endif()
   endif()
 elseif(fletch_BUILD_WITH_PYTHON AND MSVC14)
   message(WARNING "VTK Python will not build correctly on Visual Studio 2015. VTK 7.0 of higher is required.")
 endif()
 
-if (fletch_ENABLE_VTK AND VTK_WRAP_PYTHON AND VTK_SELECT_VERSION VERSION_LESS 7.0.0)
-  if (NOT fletch_PYTHON_MAJOR_VERSION VERSION_LESS 3)
-    if (fletch_BUILD_WITH_PYTHON)
+if(fletch_ENABLE_VTK AND VTK_WRAP_PYTHON AND VTK_SELECT_VERSION VERSION_LESS 7.0.0)
+  if(NOT fletch_PYTHON_MAJOR_VERSION VERSION_LESS 3)
+    if(fletch_BUILD_WITH_PYTHON)
       message(WARNING "Enabling Python 3 in VTK requires VTK 7.0 or greater")
     endif()
     # Enabling Python 3 in VTK requires VTK 7.0 or greater
@@ -237,7 +240,7 @@ list(APPEND vtk_cmake_args
   )
 
 set (VTK_PATCH_DIR ${fletch_SOURCE_DIR}/Patches/VTK/${VTK_SELECT_VERSION})
-if (EXISTS ${VTK_PATCH_DIR})
+if(EXISTS ${VTK_PATCH_DIR})
   set(VTK_PATCH_COMMAND ${CMAKE_COMMAND}
     -DVTK_PATCH_DIR=${VTK_PATCH_DIR}
     -DVTK_SOURCE_DIR=${fletch_BUILD_PREFIX}/src/VTK
