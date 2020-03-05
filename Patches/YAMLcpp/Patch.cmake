5c2a1523f54c53bba8699e4c234a62b1d7f6c0ee
David Stoup <david.stoup@kitware.com>
2020-03-05 12:14:34 -0500

    Set up patch to Yaml for Boost upgrade

--- Patches/YAMLcpp/Patch.cmake
+++ Patches/YAMLcpp/Patch.cmake
@@ -10,3 +10,8 @@ file(COPY
   ${YAMLcpp_patch}/src/node_data.cpp
   DESTINATION ${YAMLcpp_source}/src
 )
+
+file(COPY
+  ${YAMLcpp_patch}/include/yaml-cpp/node/detail/iterator.h
+  DESTINATION ${YAMLcpp_source}/include/yaml-cpp/node/detail/
+)
