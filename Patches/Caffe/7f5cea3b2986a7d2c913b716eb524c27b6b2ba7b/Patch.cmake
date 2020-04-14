62c8fbdf7edefc990ab4465c89a221302671018d
David Stoup <david.stoup@kitware.com>
2020-04-14 12:18:48 -0400

    Fix Caffe's treatment of finding Boost Python

--- Patches/Caffe/7f5cea3b2986a7d2c913b716eb524c27b6b2ba7b/Patch.cmake
+++ Patches/Caffe/7f5cea3b2986a7d2c913b716eb524c27b6b2ba7b/Patch.cmake
@@ -1,20 +1,14 @@
 # This patch is targeted at non-windows systems
 if(WIN32)
   message(FATAL_ERROR "This caffe patch is only for non-windows")
-else()
-  file(COPY
-    ${Caffe_patch}/Dependencies.cmake
-    DESTINATION
-    ${Caffe_source}/cmake/
-    )
-
-  # Fixes issue where __inbinary and __insource lists were empty.
-  file(COPY
-    ${Caffe_patch}/ConfigGen.cmake
-    DESTINATION
-    ${Caffe_source}/cmake/
-    )
 endif()
+
+# Fixes issue where __inbinary and __insource lists were empty.
+file(COPY
+  ${Caffe_patch}/ConfigGen.cmake
+  DESTINATION
+  ${Caffe_source}/cmake/
+  )
 
 file(COPY
   ${Caffe_patch}/cmake/Cuda.cmake
