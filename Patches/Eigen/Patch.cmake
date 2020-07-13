3b7d1c3aa6b5c185e7792676d021d970350238e1
David Stoup <david.stoup@kitware.com>
2020-07-13 08:23:48 -0400

    Comment out the patches so we can test for fixed bugs.

--- Patches/Eigen/Patch.cmake
+++ Patches/Eigen/Patch.cmake
@@ -4,22 +4,22 @@
 # line along with the call.
 #-
 
-message("Patching Eigen ${Eigen_patch} AND ${Eigen_source}")
-configure_file(
-  ${Eigen_patch}/cmake/FindBLAS.cmake
-  ${Eigen_source}/cmake/
-  COPYONLY
-  )
+# message("Patching Eigen ${Eigen_patch} AND ${Eigen_source}")
+# configure_file(
+#   ${Eigen_patch}/cmake/FindBLAS.cmake
+#   ${Eigen_source}/cmake/
+#   COPYONLY
+#   )
 
-# Apply language_support patch from upstream commit
-# https://bitbucket.org/eigen/eigen/commits/ba14974d054a
-configure_file(
-  ${Eigen_patch}/cmake/language_support.cmake
-  ${Eigen_source}/cmake/
-  COPYONLY
-)
+# # Apply language_support patch from upstream commit
+# # https://bitbucket.org/eigen/eigen/commits/ba14974d054a
+# configure_file(
+#   ${Eigen_patch}/cmake/language_support.cmake
+#   ${Eigen_source}/cmake/
+#   COPYONLY
+# )
 
-configure_file(
-  ${Eigen_patch}/src/LU/arch/Inverse_SSE.h
-  ${Eigen_source}/Eigen/src/LU/arch/Inverse_SSE.h
-  COPYONLY )
+# configure_file(
+#   ${Eigen_patch}/src/LU/arch/Inverse_SSE.h
+#   ${Eigen_source}/Eigen/src/LU/arch/Inverse_SSE.h
+#   COPYONLY )
