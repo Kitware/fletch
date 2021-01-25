8c4d97c78271dccc335db7a8f35d89cd267a564b
Taylor J. Cook <taylor.cook@kitware.com>
2021-01-25 12:49:34 -0500

    add basic qt downloader patch

--- CMake/fletch-tarballs.cmake
+++ CMake/fletch-tarballs.cmake
@@ -271,32 +271,19 @@ set(libkml_dlname "libkml-${libkml_version}.zip")
 list(APPEND fletch_external_sources libkml)
 
 # Qt
-# Support 4.8.6 and 5.11 optionally
 if (fletch_ENABLE_Qt OR fletch_ENABLE_VTK OR fletch_ENABLE_qtExtensions OR
     fletch_ENABLE_ALL_PACKAGES)
-  set(Qt_SELECT_VERSION 5.11.2 CACHE STRING "Select the version of Qt to build.")
-  set_property(CACHE Qt_SELECT_VERSION PROPERTY STRINGS "4.8.6" "5.11.2" "5.12.8")
+  set(Qt_SELECT_VERSION 5.12.8 CACHE STRING "Select the version of Qt to build.")
 
   set(Qt_version ${Qt_SELECT_VERSION})
   string(REPLACE "." ";" Qt_VERSION_LIST ${Qt_version})
   list(GET Qt_VERSION_LIST 0 Qt_version_major)
   list(GET Qt_VERSION_LIST 1 Qt_version_minor)
   list(GET Qt_VERSION_LIST 2 Qt_version_patch)
-  set(Qt_release_location new_archive) # official_releases or new_archive
 
-  if (Qt_version VERSION_EQUAL 5.11.2)
-    set(Qt_url "https://download.qt.io/${Qt_release_location}/qt/5.11/${Qt_version}/single/qt-everywhere-src-${Qt_version}.tar.xz")
-    set(Qt_md5 "152a8ade9c11fe33ff5bc95310a1bb64")
-  elseif (Qt_version VERSION_EQUAL 5.12.8)
-    set(Qt_release_location archive) # official_releases or new_archive
-    set(Qt_url "https://download.qt.io/${Qt_release_location}/qt/5.12/${Qt_version}/single/qt-everywhere-src-${Qt_version}.tar.xz")
+  if (Qt_version VERSION_EQUAL 5.12.8)
+    set(Qt_url "https://download.qt.io/archive/qt/5.12/${Qt_version}/single/qt-everywhere-src-${Qt_version}.tar.xz")
     set(Qt_md5 "8ec2a0458f3b8e9c995b03df05e006e4")
-  elseif (Qt_version VERSION_EQUAL 4.8.6)
-    set(Qt_release_location new_archive)
-    set(Qt_url "https://download.qt.io/${Qt_release_location}/qt/4.8/${Qt_version}/qt-everywhere-opensource-src-${Qt_version}.tar.gz")
-    set(Qt_md5 "2edbe4d6c2eff33ef91732602f3518eb")
-  else()
-    message(ERROR "Qt Version \"${Qt_version}\" Not Supported")
   endif()
 endif()
 list(APPEND fletch_external_sources Qt)
