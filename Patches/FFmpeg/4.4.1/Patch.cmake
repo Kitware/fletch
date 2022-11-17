3f5d86284b5b0bfe25aec439cf7d1990d9069650
Daniel Riehm <daniel.riehm@kitware.com>
2022-11-17 10:32:32 -0500

    Prepare FFmpeg patch

--- Patches/FFmpeg/4.4.1/Patch.cmake
+++ Patches/FFmpeg/4.4.1/Patch.cmake
@@ -9,3 +9,25 @@ configure_file(
   ${FFmpeg_source}/libavformat/
   COPYONLY
 )
+
+# Add patch to write unregistered SEI user data.
+configure_file(
+  ${FFmpeg_patch}/libx264.c
+  ${FFmpeg_source}/libavcodec/
+  COPYONLY
+)
+configure_file(
+  ${FFmpeg_patch}/libx265.c
+  ${FFmpeg_source}/libavcodec/
+  COPYONLY
+)
+configure_file(
+  ${FFmpeg_patch}/nvenc.c
+  ${FFmpeg_source}/libavcodec/
+  COPYONLY
+)
+configure_file(
+  ${FFmpeg_patch}/nvenc.h
+  ${FFmpeg_source}/libavcodec/
+  COPYONLY
+)
