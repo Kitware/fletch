30131a0f961688999288de1ccfab1cf54ddfaad1
Matt Dawkins <matt.dawkins@kitware.com>
2025-09-15 20:58:54 -0400

    Link boost to internal python when enabled

--- CMake/External_Boost.cmake
+++ CMake/External_Boost.cmake
@@ -21,18 +21,6 @@ set(_Boost_DIR_ARGS
 
 set(fletch_EXTRA_BOOST_LIBS "" CACHE STRING "Additional Boost libraries to install")
 
-if(fletch_BUILD_WITH_PYTHON)
-  set(fletch_EXTRA_BOOST_LIBS ${fletch_EXTRA_BOOST_LIBS} python)
-
-  set(_Boost_PYTHON_ARGS
-    -DPYTHON_VERSION_MAJOR=${PYTHON_VERSION_MAJOR}
-    -DPYTHON_VERSION_MINOR=${PYTHON_VERSION_MINOR}
-    -DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE}
-    -DPYTHON_INCLUDE_DIR=${PYTHON_INCLUDE_DIR}
-    -DPYTHON_LIBRARY=${PYTHON_LIBRARY}
-  )
-endif()
-
 set (Boost_PATCH_DIR ${fletch_SOURCE_DIR}/Patches/Boost/${Boost_SELECT_VERSION})
 if (EXISTS ${Boost_PATCH_DIR})
   set(Boost_PATCH_COMMAND ${CMAKE_COMMAND}
@@ -44,22 +32,22 @@ else()
 endif()
 
 ExternalProject_Add(Boost
+  DEPENDS ${Boost_DEPENDS}
   URL ${Boost_file}
   URL_MD5 ${Boost_md5}
   ${COMMON_EP_ARGS}
   PATCH_COMMAND
     ${Boost_PATCH_COMMAND}
   CONFIGURE_COMMAND ${CMAKE_COMMAND}
+    -DCMAKE_BUILD_TYPE=$<CONFIGURATION>
     -DCMAKE_VARS_FILE=${fletch_BUILD_PREFIX}/tmp/Boost/CMakeVars.cmake
     -DBoost_EXTRA_LIBS=${fletch_EXTRA_BOOST_LIBS}
     ${_Boost_DIR_ARGS}
-    ${_Boost_PYTHON_ARGS}
     -P ${fletch_SOURCE_DIR}/Patches/Boost/Configure.cmake
   BUILD_COMMAND ${CMAKE_COMMAND}
     -DCMAKE_BUILD_TYPE=$<CONFIGURATION>
     -DCMAKE_VARS_FILE=${fletch_BUILD_PREFIX}/tmp/Boost/CMakeVars.cmake
     ${_Boost_DIR_ARGS}
-    ${_Boost_PYTHON_ARGS}
     -P ${fletch_SOURCE_DIR}/Patches/Boost/Build.cmake
   INSTALL_COMMAND ${CMAKE_COMMAND}
     -DCMAKE_VARS_FILE=${fletch_BUILD_PREFIX}/tmp/Boost/CMakeVars.cmake
