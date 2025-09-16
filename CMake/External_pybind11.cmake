b8b1e3c7039442ba8758cb23bc70befbfcf3a296
Matt Dawkins <matt.dawkins@kitware.com>
2025-09-15 20:59:44 -0400

    Add other library dependencies to internal python when enabled

--- CMake/External_pybind11.cmake
+++ CMake/External_pybind11.cmake
@@ -2,8 +2,18 @@ if (NOT fletch_BUILD_CXX11)
   message(FATAL_ERROR "CXX11 must be enabled to use pybind11")
 endif()
 
+if (fletch_ENABLE_CPython)
+  add_package_dependency(
+    PACKAGE pybind11
+    PACKAGE_DEPENDENCY CPython
+  )
+endif()
+
 if (PYTHON_EXECUTABLE)
-  set(pybind_PYTHON_ARGS -DPYTHON_EXECUTABLE:FILEPATH=${PYTHON_EXECUTABLE})
+  set(PYBIND_PYTHON_ARGS -DPYTHON_EXECUTABLE:PATH=${PYTHON_EXECUTABLE})
+  set(PYBIND_PYTHON_ARGS -DPYTHON_LIBRARY:PATH=${PYTHON_LIBRARY} ${PYBIND_PYTHON_ARGS})
+  set(PYBIND_PYTHON_ARGS -DPYTHON_LIBRARY_DEBUG:PATH=${PYTHON_LIBRARY_DEBUG} ${PYBIND_PYTHON_ARGS})
+  set(PYBIND_PYTHON_ARGS -DPYTHON_INCLUDE_DIR:PATH=${PYTHON_INCLUDE_DIR} ${PYBIND_PYTHON_ARGS})
 endif()
 
 # If a patch file exists, apply it
@@ -17,6 +27,7 @@ if (EXISTS ${pybind11_patch})
 endif()
 
 ExternalProject_Add(pybind11
+  DEPENDS ${pybind11_DEPENDS}
   URL ${pybind11_url}
   URL_MD5 ${pybind11_md5}
   ${COMMON_EP_ARGS}
@@ -27,7 +38,7 @@ ExternalProject_Add(pybind11
     -DCMAKE_CXX_STANDARD=17
     # PYTHON_EXECUTABLE addded to cover when it's installed in nonstandard loc.
     # But don't pass if python isn't enabled. It will prevent pybind from finding it.
-    ${pybind_PYTHON_ARGS}
+    ${PYBIND_PYTHON_ARGS}
     -DPYBIND11_TEST:BOOL=OFF # To remove dependencies; build can still be tested manually
   )
 
